20c6147218808ef4fcf7d5922b8f727ddda79c2f
Sabiha Uzma <sabiha.uzma@student.tdsb.on.ca>
2024-12-10 13:54:40 -0500

    edited the firebase stuff for summative

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyA6I_N5znAKElfWlZMT1O6COtQ4ZgehUZ8",
+    authDomain: "summative-2411d.firebaseapp.com",
+    projectId: "summative-2411d",
+    storageBucket: "summative-2411d.firebasestorage.app",
+    messagingSenderId: "1003407905206",
+    appId: "1:1003407905206:web:e09dba7b615666f56f7117"
+  };
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
